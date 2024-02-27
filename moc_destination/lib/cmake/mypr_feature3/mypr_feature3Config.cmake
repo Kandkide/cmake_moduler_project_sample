320987ca31b2142ce9dacba4d51d7e83524b1ae5
kandkide <kandkide@gmail.com>
2024-02-28 01:48:08 +0900

    "It's boiled down so leave it alone"

--- moc_destination/lib/cmake/mypr_feature3/mypr_feature3Config.cmake
+++ moc_destination/lib/cmake/mypr_feature3/mypr_feature3Config.cmake
@@ -1,6 +1,6 @@
 # Config.cmake.in
 
 set(PROJECT_NAME "mypr_feature3")
-set(PROJECT_VERSION "")
+set(PROJECT_VERSION "1.2.3")
 set(INCLUDE_INSTALL_DIR "C:/Users/kandk/Downloads/codes_in_mouse_z6/cmake_moduler_project_sample/moc_destination/include/mypr_feature3")
 set(LIB_INSTALL_DIR "C:/Users/kandk/Downloads/codes_in_mouse_z6/cmake_moduler_project_sample/moc_destination/lib/mypr_feature3")
