2d0b1ded4423a6f7d1061805ffc842d7d93353ef
kandkide <kandkide@gmail.com>
2024-12-16 00:26:36 +0900

    "Save the snapshot to old/09"

---
+++ old/09/project_template/applications/sample_application/src/sample_application.cpp
@@ -0,0 +1,10 @@
+#include "sample_application.hpp"
+#include <iostream>
+#include <sample_component.hpp>
+#include <previous_works/previous_work.hpp>
+
+void sample_app(){
+    std::cout << "Hello from project_template!\n";
+    sample_component();
+    previous_work();
+}
